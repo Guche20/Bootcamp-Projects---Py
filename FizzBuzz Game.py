1b36a3095c6e05599662906c858224ec654945ce
Godswill Uche <72785245+Guche20@users.noreply.github.com>
2021-01-21 21:51:15 -0700

    Update FizzBuzz Game.py

--- FizzBuzz Game.py
+++ FizzBuzz Game.py
@@ -1,4 +1,15 @@
-#Write your code below this row 👇
+#This is a program that automatically prints the solution to the FizzBuzz game. 
+
+#The program prints each number from 1 to 100 in turn.
+
+#When the number is divisible by 3 then instead of printing the number it prints "Fizz".
+
+#When the number is divisible by 5, then instead of printing the number it prints "Buzz".
+
+#And if the number is divisible by both 3 and 5 e.g. 15 then instead of the number it prints "FizzBuzz"
+
+#See code below:
+
 for number in range(1,101):
   if number % 3 == 0 and number % 5 == 0:
     print ("Fizz\nBuzz")
